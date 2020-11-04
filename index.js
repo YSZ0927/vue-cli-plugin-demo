56ecc63c1c042f5fbf04176e901bd29e6797e309
shuizeng Yu <shuizengyu@91jkys.com>
2020-11-04 18:50:41 +0800

    add fs

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
-const fs = require('fs')
-const path = require('path')
-module.exports = (api) => {
-    api.registerCommand(
-        'new',
-        {
-          description: 'create new page',
-          usage: 'vue-cli-service new --name'
-        },
-        (args) => {
-            fs.mkdir(`./src/${args.name}`, (err) => {
-                console.log(err)
-                if(err === null) {
-                    
-                }
-            })
-            console.log(args.name)
-          console.log(`👋  Hello`)
-        }
-      )
+const fs = require('fs');
+const util = require('util');
+const path = require('path');
+const chalk = require('chalk');
+const mkdirAsync = util.promisify(fs.mkdir);
+
+const template = `<template>
+  <div></div>
+</template>
+
+<script>
+
+export default {
+
 }
+</script>
+
+<style lang="less" scoped>
+
+</style>
+`;
+
+module.exports = (api) => {
+  api.registerCommand(
+    'new',
+    {
+      description: 'create new page',
+      usage: 'vue-cli-service new --name'
+    },
+    async (args) => {
+      const cwd = args.cwd || process.cwd();
+      const data = fs.readFileSync(`./src/router/index.js`, 'utf-8');
+      
+      const targetDir = path.resolve(cwd, `src/${args.name}`);
+      if (fs.existsSync(targetDir)) {
+        console.error(chalk.red('目录已存在！！'));
+        return;
+      }
+      const result = await mkdirAsync(`./src/${args.name}`);
+      if (typeof result === 'undefined') {
+        fs.writeFileSync(`./src/${args.name}/index.vue`, template, 'utf-8');
+      }
+    }
+  );
+};
